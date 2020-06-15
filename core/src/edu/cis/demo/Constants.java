6daca618c8f557fe7a76980048a1bc98cb0a61b5
haerutea <tszya2020@student.cis.edu.hk>
2020-06-15 20:12:41 +0800

    added constants file

---
+++ core/src/edu/cis/demo/Constants.java
@@ -0,0 +1,8 @@
+package edu.cis.demo;
+
+public class Constants
+{
+    public static final int WIDTH = 20 * 16;
+    public static final int HEIGHT = 11 * 16;
+    public static final String MAP_FILENAME = "levelOne.tmx";
+}
